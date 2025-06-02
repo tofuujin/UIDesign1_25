b9ea2fd8534fb882fe6601d5661fb71ed564406f
tofuujin <1eavinh0pe@snu.ac.kr>
2025-06-02 10:06:23 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
- document.querySelector('#darkmode').addEventListener('click', function() {
+document.querySelectorAll('.item').forEach(function(item) {
+	item.addEventListener('click', function() {
+		this.classList.toggle('active');
+	});
+});
+
+
+document.querySelector('#darkmode').addEventListener('click', function() {
       document.getElementsByTagName('body')[0].classList.toggle('dark');
 });
